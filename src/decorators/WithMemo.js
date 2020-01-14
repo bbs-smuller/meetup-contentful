8b5136354ea6c635106eb83d3da1caa994c35abe
Sébastien Muller <smuller@big-boss-studio.com>
2020-01-14 21:18:13 +0100

    :recycle: Refactor as application functional component

---
+++ src/decorators/WithMemo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import hoist from 'hoist-non-react-statics'
+
+export default function withMemo() {
+  return WrappedComponent => {
+    const hoisted = hoist(React.memo(WrappedComponent), WrappedComponent)
+    const enhanced = {
+      // eslint-disable-next-line react/forbid-foreign-prop-types
+      propTypes: WrappedComponent.propTypes,
+      defaultProps: WrappedComponent.defaultProps,
+    }
+
+    return Object.assign(hoisted, enhanced)
+  }
+}
